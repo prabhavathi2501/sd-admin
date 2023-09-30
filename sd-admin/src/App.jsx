8bf637797981afe4e1156482a8db91d724394e5a
prabhavathi2501 <prabhashok2501@gmail.com>
2023-09-30 07:49:32 +0530

    Sencond App

---
+++ sd-admin/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react"
+import Sidebar from "./components/Sidebar"
+import Dashboard from "./components/Dashboard"
+import Create from "./components/Create"
+import Edit from "./components/Edit"
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { Navigate } from "react-router-dom"
+import NestedExample from "./components/NestedExample"
+import Receipts from "./components/NestedExample/Receipts"
+import Accounts from "./components/NestedExample/Accounts"
+import Products from "./components/NestedExample/products"
+import Staff from "./components/NestedExample/Staff"
+import UseRef from "./components/Hooks/useRef"
+function App() {
+  let [data,setData] = useState([
+    {
+      name:"Ajith",
+      username:"ajith123",
+      email:"ajith@gmail.com",
+      mobile:"12345678",
+      batch:"B100",
+    },
+    {
+      name:"Kumar",
+      username:"kum2710",
+      email:"kumar@gmail.com",
+      mobile:"098765431",
+      batch:"B107",
+    },
+    {
+      name:"prabha",
+      username:"prabha2501",
+      email:"prabha@gmail.com",
+      mobile:"979151428",
+      batch:"B102",
+    },
+    {
+      name:"Ashok",
+      username:"Ashok2710",
+      email:"ashokr@gmail.com",
+      mobile:"0987651111",
+      batch:"B104",
+    },
+  ])
+
+  return <>
+    <div id='wrapper'>
+      <BrowserRouter>
+      <Sidebar/>
+      <Routes>
+     <Route path='dashboard' element={<Dashboard data={data} setData={setData} />}/>
+      <Route path='create'element={<Create  data={data} setData={setData}/>}/>
+      <Route path='Edit/:id' element={<Edit data={data} setData={setData}/>}/>
+      <Route path='NestedExample' element={<NestedExample/>}>
+                <Route path='accounts' element={<Accounts/>}/>
+                <Route path='products' element={<Products/>}/>
+                <Route path='receipts' element={<Receipts/>}/>
+                <Route path='staffs' element={<Staff/>}/>
+            </Route>
+            <Route path='/UseRef' element={<UseRef/>}/>
+            <Route path='/*'  element={<Navigate to='/dashboard'/>}/>
+      </Routes>
+      </BrowserRouter>
+       
+    </div>
+  </>
+}
+
+export default App
